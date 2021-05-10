121f7edc521fc95f65d59405d2b80349ca5a53e7
H-J Yoo <yoohj@smu.ac.kr>
2021-05-10 22:21:24 +0900

    practice switching activities

--- app/src/main/java/com/androidapp/lotto510/ConstellationActivity.kt
+++ app/src/main/java/com/androidapp/lotto510/ConstellationActivity.kt
@@ -2,10 +2,13 @@ package com.androidapp.lotto510
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class ConstellationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constellation)
+
+        Toast.makeText(this@ConstellationActivity, "Constellation Activity", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/androidapp/lotto510/ResultActivity.kt
+++ app/src/main/java/com/androidapp/lotto510/ResultActivity.kt
@@ -2,10 +2,13 @@ package com.androidapp.lotto510
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        Toast.makeText(this, "Result Activity", Toast.LENGTH_LONG).show()
     }
 }
